57c427e879d2cb57197772d605196e1eaf3f1301
taylormcnally <22864465+taylormcnally@users.noreply.github.com>
2021-12-04 09:12:07 -0500

    remove old folder

--- animax/optimizer.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-
-def Optimizer(lr, beta1, beta2):
-		return tf.keras.optimizers.Adam(
-		learning_rate=lr, beta_1=beta1, beta_2=beta2, epsilon=1e-07, amsgrad=False,
-		name='Adam')
